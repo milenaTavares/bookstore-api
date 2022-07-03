18285f3f6eb02508d5d4d1be35a7d7cb9ac5abce
milenaTavares <milenaroas48@gmail.com>
2022-07-03 17:50:38 -0300

    Criando exceção para obj não encontrado

--- src/main/java/com/milena/bookstore/service/CategoriaService.java
+++ src/main/java/com/milena/bookstore/service/CategoriaService.java
@@ -2,6 +2,7 @@ package com.milena.bookstore.service;
 
 import com.milena.bookstore.domain.Categoria;
 import com.milena.bookstore.repositories.CategoriaRepository;
+import com.milena.bookstore.service.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,6 @@ public class CategoriaService {
 
     public Categoria findById(Integer id) {
         Optional<Categoria> obj = categoriaRepository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Categoria não encontrada! Id:" + id + "."));
     }
 }
---
+++ src/main/java/com/milena/bookstore/service/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,16 @@
+package com.milena.bookstore.service.exceptions;
+
+import org.omg.SendingContext.RunTime;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public ObjectNotFoundException(String message) {
+        super(message);
+    }
+
+    public ObjectNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
