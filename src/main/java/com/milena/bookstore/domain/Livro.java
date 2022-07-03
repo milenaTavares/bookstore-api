5b6b6ce2c08922369db912bd64cbba9a1a35f8cc
milenaTavares <milenaroas48@gmail.com>
2022-07-03 17:36:49 -0300

    Buscar categoria por id

--- src/main/java/com/milena/bookstore/domain/Livro.java
+++ src/main/java/com/milena/bookstore/domain/Livro.java
@@ -1,5 +1,7 @@
 package com.milena.bookstore.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Objects;
@@ -19,6 +21,7 @@ public class Livro implements Serializable {
 
     private String texto;
 
+    @JsonIgnore
     @ManyToOne
     @JoinColumn(name = "categoria_id")
     private Categoria categoria;
